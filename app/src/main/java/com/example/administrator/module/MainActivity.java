123955327f96bb3b94f64238bd4ea77adc6bb8e8
Administrator <657935732@qq.com>
2017-03-23 16:30:56 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/module/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.administrator.module;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017-03-23.
+ */
+
+public class MainActivity  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+}
