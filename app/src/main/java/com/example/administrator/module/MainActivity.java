9a836aeb63eebfc33512f90bf1af9f80540c8dcf
hehaisheng <657935732@qq.com>
2017-03-24 12:23:33 +0800

    第二次提交

--- app/src/main/java/com/example/administrator/module/MainActivity.java
+++ app/src/main/java/com/example/administrator/module/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity  extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //h
+        //再次测试一下
         setContentView(R.layout.layout_main);
     }
 
