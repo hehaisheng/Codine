ad2f32b546b8f200c412bc8129b035a48f7f18dd
Administrator <657935732@qq.com>
2017-03-24 10:27:22 +0800

    第一次提交

--- app/src/main/java/com/example/administrator/module/MainActivity.java
+++ app/src/main/java/com/example/administrator/module/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity  extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //h
         setContentView(R.layout.layout_main);
     }
 
