9f2907cd8a92d2fc47d5a221519e50fddac2d21c
hehaisheng <657935732@qq.com>
2017-03-28 14:54:37 +0800

    添加了一些工具类

---
+++ app/src/main/java/com/example/administrator/module/Utils/CodingRecoder.java
@@ -0,0 +1,19 @@
+package com.example.administrator.module.Utils;
+
+/**
+ * Created by Administrator on 2017-03-28.
+ */
+
+public class CodingRecoder {
+
+//    1.检查网络是否连接和连接的类型
+//    2.调度Observable线程间的切换
+//    3.多线程间的管理
+//    1.心跳包检测
+//    2.AES加密
+//    3.应用内数据清除
+
+
+
+
+}
---
+++ app/src/main/java/com/example/administrator/module/Utils/SchedulersCompat.java
@@ -0,0 +1,10 @@
+package com.example.administrator.module.Utils;
+
+/**
+ * Created by Administrator on 2017-03-28.
+ */
+
+public class SchedulersCompat {
+
+
+}
